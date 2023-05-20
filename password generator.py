8ebb36b1514221a4a126089d72514569e899c94c
ruthu <114241175+ruthujain@users.noreply.github.com>
2023-05-20 22:55:32 +0530

    Update password generator.py

--- password generator.py
+++ password generator.py
@@ -1,5 +1,6 @@
 import random
 import string
+print("RANDOM PASSWORD GENERATOR")
 
 min_len=12
 max_len=20
@@ -8,5 +9,5 @@ lower=list(string.ascii_lowercase)
 upper=list(string.ascii_uppercase)
 symbols = ['@', '#', '$', '%', '=', ':', '?', '.', '/', '~', '>','*', '(', ')', '<']
 combined_list= digits+lower+upper+symbols
-password=random.choices(combined_list, k=8)
+password=random.choices(combined_list, k=12)
 print(''.join(map(str,password)),"is the reqd password")
