40ad1e64f2ae30b9f0295b4756492d3ae09ed5dd
ruthu <114241175+ruthujain@users.noreply.github.com>
2023-07-26 18:38:05 +0530

    Update num_guesser.py

--- num_guesser.py
+++ num_guesser.py
@@ -1,6 +1,6 @@
 from random import *
 ran=randint(0,9)
-print(ran)
+
 print("WELCOME TO THE NUMBER GUESSING GAME!!!")
 n=int(input("Enter the number of chances you would like to take: "))
 for i in range(1,n+1):
